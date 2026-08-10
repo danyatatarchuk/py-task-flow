142d4e35e4149718baab24a6146d31e8f8aec491
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-10 13:01:28 +0300

    Configure Worker in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Position, Worker
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = ("id", "username", "email", "position", "is_staff")
+    list_filter = ("position", "is_staff", "is_active")
--- tasks/models.py
+++ tasks/models.py
@@ -14,6 +14,8 @@ class Worker(AbstractUser):
         Position,
         on_delete=models.CASCADE,
         related_name="workers",
+        null=True,
+        blank=True,
     )
 
     def __str__(self):
