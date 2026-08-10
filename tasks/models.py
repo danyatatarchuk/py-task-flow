8cbc956655c77c0b40dffeb3e3ac946e4338bc2b
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-10 12:41:37 +0300

    Add custom Worker user model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -6,3 +7,14 @@ class Position(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+    )
+
+    def __str__(self):
+        return self.username
