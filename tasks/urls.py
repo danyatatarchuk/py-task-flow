b5b09d45357f79538b74360e19da6ee842f5f10c
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-11 01:32:40 +0300

    Add task detail view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tasks.views import TaskListView
+from tasks.views import TaskDetailView, TaskListView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from tasks.models import Task
 
@@ -7,3 +7,9 @@ class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "tasks"
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "tasks/task_detail.html"
+    context_object_name = "task"
